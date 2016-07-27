36c8ecbb5fe7b3e72fbe9d8a631469d35234c447
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2016-07-27 11:53:23 +1000

    Initial attempt at robust node coords

--- boogie/spectral.py
+++ boogie/spectral.py
@@ -2,6 +2,7 @@ from scipy import sparse
 import numpy as np
 import networkx as nx
 import warnings
+import itertools
 
 
 def pagerank_power(Trans, damping=0.85, max_iter=int(1e5)):
@@ -69,23 +70,55 @@ def node_coordinates(graph, remove_nodes=None, nodelist=None):
     return x, y, z, A, names
 
 
+def coo_mat_concat(matrices, spformat='csr'):
+    ii = []
+    jj = []
+    dat = []
+    locr = 0
+    locc = 0
+    for mat in matrices:
+        coo = sparse.coo_matrix(mat, shape=mat.shape)
+        ii.append(coo.row)
+        jj.append(coo.col)
+        dat.append(coo.data)
+        locr += coo.shape[0]
+        locc += coo.shape[1]
+    coo = sparse.coo_matrix((np.concatenate(dat),
+                             (np.concatenate(ii), np.concatenate(jj))))
+    out = getattr(coo, 'to' + spformat)()
+    return out
+
+
 def node_coordinates_robust(graph):
     xs, ys, zs, As, namess = [], [], [], [], []
-    for cc in nx.connected_component_subgraphs(graph.to_undirected()):
+    for cc in nx.connected_components(graph.to_undirected()):
         if len(cc) == 1:
             x, y, z = [0], [0], [0]
-            A = [[0]]
-            names = cc.nodes()
+            A = np.array([[0]])
+            names = list(cc)
         elif len(cc) == 2:
             x, y, z = [0, 1], [0, 1], [0, 1]
-            n1, n2 = cc.nodes()
-            A = np.array([[0, 1], [1, 0]]) * cc[n1][n2].get('weight', 1)
-            names = cc.nodes()
+            n1, n2 = list(cc)
+            A = np.array([[0, 1], [1, 0]]) * graph[n1][n2].get('weight', 1)
+            names = list(cc)
         else:
-            x, y, z, A, names = node_coordinates(cc)
+            x, y, z, A, names = node_coordinates(nx.subgraph(graph, cc))
         xs.append(x)
         ys.append(y)
         zs.append(z)
         As.append(A)
         namess.append(names)
-    return xs, ys, zs, As, namess
+    for coord in [xs, ys, zs]:
+        loc = 0
+        for i, arr in enumerate(coord):
+            arr = np.asanyarray(arr)
+            scale = np.sqrt(arr.size - 0.99)
+            coord[i] = ((arr - np.min(arr)) / np.max(arr) *
+                        scale + loc)
+            loc += 1.05 * scale
+    x = np.concatenate(xs)
+    y = np.concatenate(ys)
+    z = np.concatenate(zs)
+    A = coo_mat_concat(As)
+    names = list(itertools.chain(*namess))
+    return x, y, z, A, names
